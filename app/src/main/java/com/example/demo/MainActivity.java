efc99731f5b58ece0347ce76f2090d3ddd25b588
dadahuan <1136086940@qq.com>
2019-07-25 09:07:52 +0800

    第三次提交

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
@@ -10,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,5 +54,48 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
     }
-
+    /**
+     * 
+     * 点击事件
+     * @anthor:  LiuHuan
+     * 2019/7/12 14:45
+     */
+    @OnClick({R.id.txt_set, R.id.sv_search, R.id.ll_user, R.id.btn_fighit, R.id.txt_link, R.id.ll_wireless, R.id.txt_switch, R.id.ll_bluetooth, R.id.ll_honeycomb, R.id.txt_hsswitch, R.id.ll_hotspot, R.id.ll_inform, R.id.ll_sound, R.id.ll_notdisturb, R.id.ll_screentime})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.txt_set:
+                Intent intent=new Intent(this,ShowActivity.class);
+//                intent.putExtra("id",);
+                startActivity(intent);
+                break;
+            case R.id.sv_search:
+                break;
+            case R.id.ll_user:
+                break;
+            case R.id.btn_fighit:
+                break;
+            case R.id.txt_link:
+                break;
+            case R.id.ll_wireless:
+                break;
+            case R.id.txt_switch:
+                break;
+            case R.id.ll_bluetooth:
+                break;
+            case R.id.ll_honeycomb:
+                break;
+            case R.id.txt_hsswitch:
+                break;
+            case R.id.ll_hotspot:
+                break;
+            case R.id.ll_inform:
+                break;
+            case R.id.ll_sound:
+                break;
+            case R.id.ll_notdisturb:
+                break;
+            case R.id.ll_screentime:
+                break;
+        }
+    }
 }
